bf5b03c98ddc4822a71020ec8728f9914cc7bd42
HomieZ09 <phumsoen123@gmail.com>
2024-07-17 01:11:08 +0700

    fixed ui

--- src/app/components/storys/2.tsx
+++ src/app/components/storys/2.tsx
@@ -4,11 +4,11 @@ export default function ButterflyEffect() {
     return (
         <>
             {/* story */}
-            <div className="flex flex-col items-center justify-center h-full w-full">
-                <div className="relative w-[95vw] h-[400px] m-[-4rem]">
+            <div className="flex flex-col items-center justify-center h-full w-full mt-[-4rem]">
+                <div className="relative w-[90%] h-[400px] my-[-4rem]">
                     <Image priority src="/mystery-market/assets/images/butterflyeffect.png" fill className="object-contain" alt="Mystery Market Logo" />
                 </div>
-                <div className="space-y-[-0.3rem]">
+                <div className="space-y-[-0.2rem]">
                     <p className="text-left w-full text-2xl bg-gradient-to-r from-[#E4CCFF] to-[#C2DBFF] inline-block text-transparent bg-clip-text font-bold italic">THE BUTTERFLY EFFECT.</p>
                     <p className="pb-8 text-lg font-bold text-left w-full">ทฤษฎีผีเสื้อขยับปีก</p>
                     <p className="text-left w-full pb-8">
