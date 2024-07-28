7eeca03053f6ae9f729780104296e309d193133d
inspirezuza <inspire7220@gmail.com>
2024-07-29 00:39:22 +0700

    chore: Update metadataBase URL in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ const ibm = IBM_Plex_Sans_Thai({ weight: ["400", "700"], subsets: ["thai"] });
 export const metadata: Metadata = {
   title: "MYSTERY MARKET",
   description: "ค้นหาดอกไม้ของคุณในตลาดน้ำราตรีแห่งแสงจันทร์",
-  manifest: "/mystery-market/site.webmanifest",
+  metadataBase: new URL("https://mystery-market.netlify.app/mystery-market"),
+  // manifest: "/mystery-market/site.webmanifest",
 };
 
 export default function RootLayout({
